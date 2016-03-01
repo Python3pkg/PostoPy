fec6c4416544ba379bc5378da22640f42a787e61
Rahman <mousavian.rahman@gmail.com>
2016-03-01 16:14:37 +0800

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='postopy',
+      version='0.1',
+      description='Posto Express API in Python',
+      url='http://github.com/mousaian/PostoPy',
+      author='Rahman Mousavian',
+      author_email='Mousavian.Rahman@gmail.com',
+      license='MIT',
+      packages=('postopy'),
+      zip_safe=False)
